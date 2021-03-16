c019ca8ba967cd18a88d6bbb01d20f1d0c516454
Sandro <SGamrekelashvili@gmail.com>
2021-03-16 13:24:36 +0400

    dda

--- src/Client/client.js
+++ src/Client/client.js
@@ -5,14 +5,14 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: "https://smartacademy-back.herokuapp.com/graphql",
   cache: new InMemoryCache(),
-  fetchOptions: {
-    mode: `no-cors`,
-  },
-  // credentials: "include",
-  headers: {
-    // authorization: token ? `Bearer ${token}` : " ",
-    "Access-Control-Allow-Origin": "*",
-  },
+  // fetchOptions: {
+  //   mode: `no-cors`,
+  // },
+  // // credentials: "include",
+  // headers: {
+  //   // authorization: token ? `Bearer ${token}` : " ",
+  //   "Access-Control-Allow-Origin": "*",
+  // },
 });
 
 export default client;
